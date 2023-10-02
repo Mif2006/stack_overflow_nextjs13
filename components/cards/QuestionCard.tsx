f5fe6f854faccfe6d8ded3e695505c43247ff7c1
Mif2006 <frolov.milan@gmail.com>
2023-10-02 13:32:57 +0300

    Home page done

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
-import React from 'react'
-import RenderTag from '../shared/RenderTag';
-import Metric from '../shared/Metric';
-import { formatAndDivideNumber, getTimestamp } from '@/lib/utils';
+import Link from "next/link";
+import React from "react";
+import RenderTag from "../shared/RenderTag";
+import Metric from "../shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
 
-interface QuestionProps {
+interface Props {
   _id: string;
   title: string;
   tags: {
@@ -30,8 +30,8 @@ const QuestionCard = ({
   upvotes,
   views,
   answers,
-  createdAt
-}: QuestionProps) => {
+  createdAt,
+}: Props) => {
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -39,24 +39,24 @@ const QuestionCard = ({
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
           </span>
+
           <Link href={`/question/${_id}`}>
-            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
-              {title} 
-            </h3>
+            <h3 className="h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">{title}</h3>
           </Link>
         </div>
 
-        {/* If signed in add edit delete actions */}
-      </div>
-      
-      <div className="mt-3.5 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
-        ))}
+        {/* Add edit delete actions */}
       </div>
 
-      <div className="flex-between mt-6 w-full flex-wrap gap-3">
-          <Metric 
+
+    <div className="mt-3.5 flex flex-wrap gap-2">
+      {tags.map((tag) => (
+        <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+      ))}
+    </div>
+
+    <div className="flex-between mt-6 w-full flex-wrap gap-3">
+    <Metric
             imgUrl="/assets/icons/avatar.svg"
             alt="user"
             value={author.name}
@@ -65,32 +65,31 @@ const QuestionCard = ({
             isAuthor
             textStyles="body-medium text-dark400_light700"
           />
-
-          <Metric 
+          <Metric
             imgUrl="/assets/icons/like.svg"
             alt="Upvotes"
             value={formatAndDivideNumber(upvotes)}
             title=" Votes"
             textStyles="small-medium text-dark400_light800"
           />
-          <Metric 
+          <Metric
             imgUrl="/assets/icons/message.svg"
             alt="message"
             value={formatAndDivideNumber(answers.length)}
             title=" Answers"
             textStyles="small-medium text-dark400_light800"
           />
-          <Metric 
+          <Metric
             imgUrl="/assets/icons/eye.svg"
             alt="eye"
             value={formatAndDivideNumber(views)}
             title=" Views"
             textStyles="small-medium text-dark400_light800"
           />
-      </div>
-      
     </div>
-  )
-}
+     
+    </div>
+  );
+};
 
-export default QuestionCard
+export default QuestionCard;
